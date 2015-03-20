c0995982f8a664b4c539ee2d06b1ce6e15e24756
Lasse <lassesl93@gmail.com>
2015-03-20 08:51:10 +0100

    Test

--- src/DBLayer/DbConnectionTest.java
+++ src/DBLayer/DbConnectionTest.java
@@ -11,7 +11,7 @@ public class DbConnectionTest {
 		DbConnection dbCon = DbConnection.getInstance();
 		if(dbCon != null)
 		{
-			System.out.println("Conecction to DB is ok");
+			System.out.println("Connection to DB is ok");
 		}
 		else{
 		    fail("Can not connect to the DB");
